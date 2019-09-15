3a82b2f19961d857ba07d1cbbeec79933ed52d43
Prashanth Rao <psprao95@gmail.com>
2019-09-15 18:47:20 -0500

    process class added

--- src/Process.java
+++ src/Process.java
@@ -1,15 +1,42 @@
+import java.util.HashMap;
+import java.util.concurrent.Callable;
 
-public class Process {
-private int uid;
+class Process implements Callable{
+	
+	private int uid;
+	private int anti_clockwise_neighbor;
+	private int num_wait_rounds;
+	 String status;
+	int token;
+	private HashMap<Integer,Integer> map;
 
-
-public Process(int a)
+public Process(int uid, int anti_clockwise_neighbor, int token, int num_wait_rounds , String status, HashMap<Integer,Integer> map)
 {
-	this.uid=a;
-	Token token=new Token(uid);
+	this.uid=uid;
+	this.anti_clockwise_neighbor=anti_clockwise_neighbor;
+	this.token=token;
+	this.status=status;
+	this.num_wait_rounds=num_wait_rounds;
+	this.map=map;
+	
+
 }
 public int getUniqueID()
 {
 	return uid;
 }
+
+@Override
+public String call()
+{
+	try
+	{
+		System.out.println("Hi I am process "+uid);
+	}
+	catch(Exception ex)
+	{
+		System.out.println(ex);
+	}
+}
+public 
 }
--- src/VariableSpeedsAlgorithm.java
+++ src/VariableSpeedsAlgorithm.java
@@ -2,27 +2,32 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.stream.*;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
-
-
-
-
-
 
 
 public class VariableSpeedsAlgorithm {
  
 	public static void main(String[] args) {
-		try {
+		try 
+		{
 			Scanner scanner=new Scanner(new File("/users/psprao/downloads/input.dat"));
+			
+			// number of processes (n)
 			int noOfThreads=scanner.nextInt();
 			int[] uids=new int[noOfThreads];
-			int i=0;
-			while(scanner.hasNextInt()) {
+			System.out.println("Number of processes: "+noOfThreads);
+			
+			// take the uids of the processes i.e. uids[]
+			for(Integer i=0;i<noOfThreads;i++)
+			{
 				uids[i]=scanner.nextInt();
-				System.out.println(uids[i]);
+				
 			}
+			System.out.println("Input UIDs of processes: "+Arrays.toString(uids));
+			
+		String leader="unknown";
 			
 			
 		} catch (Exception e) {
